87612c2dfda2a91f56652cd1736c5dc2d49556f4
Issa Ashwash <iashwash@bitsighttech.com>
2013-11-08 12:30:13 -0800

    Exposing python packages so other cookbooks can override

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,28 @@
+
+
+default[:wal_e][:packages] = [
+  "python-setuptools",
+  "python-dev",
+  "lzop",
+  "pv",
+  "postgresql-client",
+  "libevent-dev",
+  "daemontools"
+
+]
+
+default[:wal_e][:pips] = [
+  "gevent",
+  "argparse",
+  "boto"
+]
+
 default[:wal_e][:git_version]         = "v0.6.5"
 
 default[:wal_e][:env_dir]             = '/etc/wal-e'
-default[:wal_e][:aws_access_key]      = ''
-default[:wal_e][:aws_secret_key]      = ''
-default[:wal_e][:s3_prefix]           = ''
+default[:wal_e][:aws_access_key]      = 'AKIAIMFJABPVP5VSF36A'
+default[:wal_e][:aws_secret_key]      = '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'
+default[:wal_e][:s3_prefix]           = 's3://com.bitsighttech.backup/emwal'
 
 default[:wal_e][:base_backup][:minute]  = '0'
 default[:wal_e][:base_backup][:hour]    = '0'
@@ -13,4 +32,4 @@ default[:wal_e][:base_backup][:weekday] = '1'
 
 default[:wal_e][:user]                = 'postgres'
 default[:wal_e][:group]               = 'postgres'
-default[:wal_e][:pgdata_dir]          = '/var/lib/postgresql/9.2/main/'
+default[:wal_e][:pgdata_dir]          = '/var/lib/postgresql/9.1/main/'
